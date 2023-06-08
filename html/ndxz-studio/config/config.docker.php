3e426326fc3efbf475bd1826f69b9775dced88f6
Florian Jenett <florian.jenett@hs-mainz.de>
2023-06-08 10:51:01 +0200

    added default config

---
+++ html/ndxz-studio/config/config.docker.php
@@ -0,0 +1,9 @@
+<?php  if (!defined('SITE')) exit('No direct script access allowed');
+
+$indx['db'] 		= 'ndxz-database';
+$indx['user'] 		= 'ndxz-user';
+$indx['pass'] 		= 'ndxz-password';
+$indx['host'] 		= 'idxz-mariadb';
+$indx['sql']		= 'mysql';
+
+?>
